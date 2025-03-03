5ef1a682bd7a49bf3f871999a992b552314c5eca
b1ng-b1ng <rongthien093@gmail.com>
2025-03-03 21:05:47 +0700

    Add files via upload

--- bai6.c
+++ bai6.c
@@ -1,21 +1,11 @@
 #include<stdio.h>
 #include<math.h>
 	int main(){
-	float diem;
-	scanf("%f",&diem);
-	if(diem<5){
-		printf("yeu\n");
-	}	
-    else if (diem >= 5 && diem < 7) {
-        printf("Trung binh\n");
-    } else if (diem >= 7 && diem < 8) {
-        printf("Kha\n");
-    } else if (diem >= 8 && diem < 9) {
-        printf("Gioi\n");
-    } else if (diem >= 9 && diem <= 10) {
-        printf("Xuat sac\n");
-    } else {
-        printf("sai");
-    }
-				return 0;
-	}
+	float n;
+	scanf("%f",&n);
+	printf("%f\n",n*n);
+	printf("%f\n",n*n*n);
+	printf("%f\n",n*n*n*n);
+	return 100;
+}
+
