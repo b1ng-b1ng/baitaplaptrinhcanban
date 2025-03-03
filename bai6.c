5d057bb3b431adf5bb6c4eda0fc163416517a428
b1ng-b1ng <rongthien093@gmail.com>
2025-03-03 20:56:39 +0700

    Update bai6.c

--- bai6.c
+++ bai6.c
@@ -6,13 +6,13 @@
 	if(n<5){
 		printf("yeu\n");
 	}	
-    else if (diem >= 5 && diem < 7) {
+    else if (n >= 5 && n < 7) {
         printf("Trung binh\n");
-    } else if (diem >= 7 && diem < 8) {
+    } else if (n >= 7 && n < 8) {
         printf("Kha\n");
-    } else if (diem >= 8 && diem < 9) {
+    } else if (n >= 8 && n < 9) {
         printf("Gioi\n");
-    } else if (diem >= 9 && diem <= 10) {
+    } else if (n >= 9 && n <= 10) {
         printf("Xuat sac\n");
     } else {
         printf("sai");
